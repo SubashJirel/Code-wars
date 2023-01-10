0dfce498474a6994e70ed4f6c7b48c748c7d3fe7
Subash Jirel <subashjrl@gmail.com>
2023-01-10 23:06:42 +0545

    get middle

--- 8kyu/12.hoopCount.js
+++ 8kyu/12.hoopCount.js
@@ -1,8 +1,13 @@
-function hoopCount (n) {
-    //your code goes here    
-    if(n<10) {return "Keep at it until you get it"}
-        
-      
-      else{return "Great, now move on to tricks"}
-        
-} 
+function getMiddle(s)
+{
+  //Code goes here!
+  let length = s.length
+  if(length%2 !=0) {
+    return s[(length+1)/2-1]
+  }
+  else {
+    return s[(length/2)-1] + s[length/2]
+  }
+}
+console.log(getMiddle("middle"))
+console.log(getMiddle("tests"))
