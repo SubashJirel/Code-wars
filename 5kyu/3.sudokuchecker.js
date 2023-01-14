5dc8bc99bb8c461a47947374aa7e099563eb355f
Subash Jirel <subashjrl@gmail.com>
2023-01-14 17:07:48 +0545

    middle element index

--- 5kyu/3.sudokuchecker.js
+++ 5kyu/3.sudokuchecker.js
@@ -61,7 +61,8 @@ function doneOrNot(board){
 
   }
 
-  let board = [[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+  let board = [
+  [5, 3, 4, 6, 7, 8, 9, 1, 2], 
   [6, 7, 2, 1, 9, 5, 3, 4, 8],
   [1, 9, 8, 3, 4, 2, 5, 6, 7],
   [8, 5, 9, 7, 6, 1, 4, 2, 3],
@@ -83,3 +84,30 @@ function doneOrNot(board){
   [3, 0, 0, 4, 8, 1, 1, 7, 9]]
 
   console.log(doneOrNot(board))
+
+  //Other code
+  function doneOrNot_othercode(rows){
+
+    var columns = []
+    ,    blocks = [];
+    
+    for (var i = 0; i < 9; i++) {
+      columns[i] = [];
+      
+      for (var j = 0; j < 9; j++) {
+        var k = Math.floor(i / 3) + Math.floor(j / 3) * 3;
+        blocks[k] = blocks[k] || [];
+        
+        blocks[k].push(rows[i][j]);
+        columns[i].push(rows[j][i]);
+      }
+    }
+    
+    var is_valid_row = (row) => row.slice(0).sort((a, b) => a - b).join('') == '123456789';
+    
+    var is_valid = rows.every(is_valid_row) 
+      && columns.every(is_valid_row) 
+      && blocks.every(is_valid_row);
+    
+    return is_valid ? 'Finished!' : 'Try again!';
+  }
