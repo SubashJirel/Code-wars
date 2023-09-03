edaff6a7fd71d0ebe72ce961690032d4c6af1cff
Subash Jirel <subashjrl@gmail.com>
2023-09-03 22:34:57 +0545

    you only need one

---
+++ 8kyu/50.youOnlyNeedOne.js
@@ -0,0 +1,15 @@
+/*
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not.
+*/
+
+function check(a, x) {
+  // your code here
+  return a.some((val) => val === x);
+}
+
+console.log(check(['t', 'e', 's', 't'], 'e'));
+console.log(check(['what', 'a', 'great', 'kata'], 'kat'));
